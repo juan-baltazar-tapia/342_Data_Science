a35e65099c2297befb3d2eefbf89c2921b32b8e7
c-deras <coderasr@gmail.com>
2023-09-29 17:33:45 -0400

    update2

--- Cleaning.R
+++ Cleaning.R
@@ -50,58 +50,62 @@ clean$CONTRIBUTING.FACTOR.VEHICLE.3[clean$CONTRIBUTING.FACTOR.VEHICLE.3 == 1] <-
 clean$CONTRIBUTING.FACTOR.VEHICLE.4[clean$CONTRIBUTING.FACTOR.VEHICLE.4 == 1] <- NA
 clean$CONTRIBUTING.FACTOR.VEHICLE.5[clean$CONTRIBUTING.FACTOR.VEHICLE.5 == 1] <- NA
 
-#select variables of interests and convert
-clean <- clean %>% 
-  select(NUMBER.OF.PERSONS.INJURED, NUMBER.OF.PERSONS.KILLED, CONTRIBUTING.FACTOR.VEHICLE.1, CONTRIBUTING.FACTOR.VEHICLE.2,
-         CONTRIBUTING.FACTOR.VEHICLE.3, CONTRIBUTING.FACTOR.VEHICLE.4, CONTRIBUTING.FACTOR.VEHICLE.5) %>% 
-  drop_na(CONTRIBUTING.FACTOR.VEHICLE.1, NUMBER.OF.PERSONS.INJURED, NUMBER.OF.PERSONS.KILLED) %>% 
-  mutate(Injured = ifelse(NUMBER.OF.PERSONS.INJURED > 0, "Yes", "No")) %>% 
-  mutate(Killed = ifelse(NUMBER.OF.PERSONS.KILLED > 0, "Yes", "No")) %>% 
-  select(-c(NUMBER.OF.PERSONS.INJURED, NUMBER.OF.PERSONS.KILLED))
+#select variables of interests and convert target variable to categorical
+clean1 <- clean %>% 
+  select(CONTRIBUTING.FACTOR.VEHICLE.1, CONTRIBUTING.FACTOR.VEHICLE.2, CONTRIBUTING.FACTOR.VEHICLE.3, 
+         CONTRIBUTING.FACTOR.VEHICLE.4, CONTRIBUTING.FACTOR.VEHICLE.5, NUMBER.OF.PERSONS.INJURED, NUMBER.OF.PERSONS.KILLED) %>% 
+  drop_na(CONTRIBUTING.FACTOR.VEHICLE.1, NUMBER.OF.PERSONS.INJURED, NUMBER.OF.PERSONS.KILLED) #%>% 
+  #mutate(Injured = ifelse(NUMBER.OF.PERSONS.INJURED > 0, "Yes", "No")) %>% 
+  #mutate(Killed = ifelse(NUMBER.OF.PERSONS.KILLED > 0, "Yes", "No")) %>% 
+  #select(-c(NUMBER.OF.PERSONS.INJURED, NUMBER.OF.PERSONS.KILLED))
 
 #categorize
-clean2 <- clean
+clean2 <- clean1
 index <- clean2[1]
 
 for (i in 1:dim(index)) {
-  if (clean2[1][i] %in% Driver_Factors == TRUE) {
-    index[i] <-  "driver" 
-  } else if (clean2[1][i] %in% Environmental_Factors == TRUE) {
-    index[i] <- "environmental"
-  } else if (clean2[1][i] %in% Road_Factors == TRUE) {
-    index[i] <- "road"
-  }else if (clean2[1][i] %in% Tech_Factors == TRUE) {
-    index[i] <- "tech"
-  } else if (clean2[1][i] %in% Vehicle_Factors == TRUE) {
-    index[i] <- "vehicle"
+  if (index[i,] %in% Driver_Factors == TRUE) {
+    index[i,] <- "driver" 
+  } else if (index[i,] %in% Environmental_Factors == TRUE) {
+    index[i,] <- "environmental"
+  } else if (index[i,] %in% Road_Factors == TRUE) {
+    index[i,] <- "road"
+  }else if (index[i,] %in% Tech_Factors == TRUE) {
+    index[i,] <- "tech"
+  } else if (index[i,] %in% Vehicle_Factors == TRUE) {
+    index[i,] <- "vehicle"
   } else {}
 }
 
-clean1 <- read.csv("clean.csv") %>% 
+clean2 <- clean1 %>% 
+  mutate(index) %>% 
+  select(CONTRIBUTING.FACTOR.VEHICLE.1, NUMBER.OF.PERSONS.INJURED, NUMBER.OF.PERSONS.KILLED)
+
+ggplot(clean2, aes(NUMBER.OF.PERSONS.INJURED)) + geom_bar(aes(fill = CONTRIBUTING.FACTOR.VEHICLE.1))
+
+clean3 <- read.csv("clean.csv") %>% 
   drop_na(Injured, Killed) %>% 
   select(-X) %>% 
   mutate(result = ifelse(Injured == "No" & Killed == "No", "No one hurt", "Someone Hurt"))
 
-ggplot(clean1, aes(CONTRIBUTING.FACTOR.VEHICLE.1)) + geom_bar(aes(fill = result ), position = "fill")
+ggplot(clean3, aes(CONTRIBUTING.FACTOR.VEHICLE.1)) + geom_bar(aes(fill = result ))
+ggplot(clean3, aes(CONTRIBUTING.FACTOR.VEHICLE.1)) + geom_bar(aes(fill = result ), position = "fill")
 
-Combined_Table <- clean1 %>% 
+Combined_Table <- clean3 %>% 
   group_by(CONTRIBUTING.FACTOR.VEHICLE.1, Injured, Killed) %>% 
   summarise(Count = n())
 
-Killed_Table <- clean1 %>% 
+Killed_Table <- clean3 %>% 
   select(CONTRIBUTING.FACTOR.VEHICLE.1, Killed) %>% 
   group_by(CONTRIBUTING.FACTOR.VEHICLE.1, Killed) %>% 
   summarize(Count = n()) %>% 
   spread(Killed, Count)
 
-Injured_Table <- clean1 %>% 
+Injured_Table <- clean3 %>% 
   select(CONTRIBUTING.FACTOR.VEHICLE.1, Injured) %>% 
   group_by(CONTRIBUTING.FACTOR.VEHICLE.1, Injured) %>% 
   summarize(Count = n()) %>% 
   spread(Injured, Count)
-
-ggplot(clean1, aes(CONTRIBUTING.FACTOR.VEHICLE.1)) + geom_bar(aes(fill = result ), position = "fill")
-
 
 #library(usethis)
 #use_git_config(user.name = "c-deras", user.email = "coderasr@gmail.com")
