6674c167bf8fd1a5d78cc4f1ededffbb7ce45e03
MrFxde <juan.space14@gmail.com>
2023-10-10 13:07:17 -0400

    Added cart models R file, and paritionaing R file

---
+++ partitioning.R
@@ -0,0 +1,23 @@
+install.packages("caret")
+library(caret)
+
+#setting the seed
+set.seed()
+
+#Partitioning the data
+part01 <- 
+
+d.train <- d[ part01, ] 
+d.test <- d[ -part01, ]
+
+# Validate the partition
+dim(d)
+
+t.test(x = )
+t.test
+
+# three different variables
+if p value < 0.05 or 0.10 (which ever is chosen)
+chi-square, p value should be close to 100% or 1.00
+
+
