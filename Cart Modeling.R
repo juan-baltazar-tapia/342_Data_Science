a205448ed27252cefbc8c91721e5c0768256e1e1
c-deras <coderasr@gmail.com>
2023-10-17 13:27:20 -0400

    adding more columns woo

--- Cart Modeling.R
+++ Cart Modeling.R
@@ -1,9 +1,16 @@
+library(tidyverse)
+library(rpart); library(rpart.plot)
+
+clen <- read.csv("clean4cart.csv") %>% select(-X)
+
 cleann <- clean %>% 
   select(CRASH.DATE, CRASH.TIME, BOROUGH, CONTRIBUTING.FACTOR.VEHICLE.1, NUMBER.OF.PERSONS.INJURED, NUMBER.OF.PERSONS.KILLED) %>% 
   drop_na() %>% 
-  mutate(Hurt = ifelse(NUMBER.OF.PERSONS.KILLED > 0 & NUMBER.OF.PERSONS.INJURED > 0, 1, 0)) %>% 
-  mutate(CRASH.DATE = format(as.Date(CRASH.DATE, "%m/%d/%Y"), format = "%m-%d-%y")) %>% 
-  select(-c(NUMBER.OF.PERSONS.KILLED, NUMBER.OF.PERSONS.INJURED))
+  mutate(CRASH.DATE = format(as.Date(CRASH.DATE, "%m/%d/%Y"), format = "%m")) %>% 
+  mutate(CRASH.TIME = format(hour, '%H')) %>% 
+  mutate(CONTRIBUTING.FACTOR = clen$CONTRIBUTING.FACTOR.VEHICLE.1) %>% 
+  mutate(HURT.STATUS = ifelse(NUMBER.OF.PERSONS.KILLED > 0 | NUMBER.OF.PERSONS.INJURED > 0, 1, 0)) %>% 
+  select(-c(NUMBER.OF.PERSONS.KILLED, NUMBER.OF.PERSONS.INJURED, CONTRIBUTING.FACTOR.VEHICLE.1))
 
 for (i in 1:length(cleann$CONTRIBUTING.FACTOR.VEHICLE.1)) {
   if (cleann$CONTRIBUTING.FACTOR.VEHICLE.1[i] %in% Driver_Factors == TRUE) {
@@ -19,5 +26,3 @@ for (i in 1:length(cleann$CONTRIBUTING.FACTOR.VEHICLE.1)) {
   } else {}
   print(i)
 }
-
-  
